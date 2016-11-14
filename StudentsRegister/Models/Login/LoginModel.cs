e5b32d0bb6aebbd2bd3a1de84d5a1812e979cf8d
Krzysztof Truskolaski <truskolaski.krzysztof@gmail.com>
2016-11-14 23:36:50 +0100

    Added navbar, login modal (without functionality)

---
+++ StudentsRegister/Models/Login/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace StudentsRegister.Models.Login
+{
+    public class LoginModel
+    {
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [DisplayName("Password")]
+        public string Password { get; set; }
+    }
+}
